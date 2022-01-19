4e147e8e406c62cfbee0855e3a95fe6b0a8d7172
Gyan Vardhan <gyansingh1997@gmail.com>
2022-01-20 00:31:11 +0530

    vercel changes

--- next.config.js
+++ next.config.js
@@ -6,7 +6,17 @@ module.exports = withCSS(withSass({
     webpack(config, {dev}) {
         if (config.mode === 'production') {
             if (Array.isArray(config.optimization.minimizer)) {
-                config.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({}));
+                config.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({
+                    assetNameRegExp: /\.optimize\.css$/g,
+                    cssProcessor: require('cssnano'),
+                    cssProcessorPluginOptions: {
+                        preset: [
+                            "default",
+                            { discardComments: { removeAll: true } }
+                        ]
+                    },
+                    canPrint: true
+                }));
             }
         }
         return config;
